6f1ac335f9845644e7dbb79c29012dd9d8357b12
hnnsly <hynslei@ya.ru>
2024-04-08 23:42:01 +0300

    hotfixes

--- bot/handlers/jobs.py
+++ bot/handlers/jobs.py
@@ -42,7 +42,7 @@ async def send_child_jobs(message: Message):
 
 @router.message(
     # UserDB.get_user_age(F.from_user.id) == 30 and
-    # F.text.lower() in ArticleDB.get_adult_jobs_names())
+    # F.text in ArticleDB.get_adult_jobs_names())
 
     lambda message: UserDB.get_user_age(message.from_user.username) == 30 and message.text in ArticleDB.get_adult_jobs_names(),
     StateFilter(None)
@@ -57,13 +57,13 @@ async def send_chosen_adult_job(message: Message):
 
 @router.message(
     # UserDB.get_user_age(F.from_user.id) == 30 and
-    # F.text.lower() in ArticleDB.get_adult_jobs_names())
+    # F.text in ArticleDB.get_adult_jobs_names())
 
     lambda message: UserDB.get_user_age(message.from_user.username) == 20 and message.text in ArticleDB.get_child_jobs_names(),
     StateFilter(None)
 )
 async def send_chosen_child_job(message: Message):
-    response = ArticleDB.get_article_by_name(message.text.lower())
+    response = ArticleDB.get_article_by_name(message.text)
     await message.answer(
         text=response.content,
         reply_markup=inline_link("Cсылка",response.link) if response.link is not None else None
@@ -71,7 +71,7 @@ async def send_chosen_child_job(message: Message):
 
 # @router.message(
 #     #UserDB.get_user_age(F.from_user.id) == 20 and
-#     #F.text.lower() in ArticleDB.get_adult_jobs_names()
+#     #F.text in ArticleDB.get_adult_jobs_names()
 #     StateFilter(None)
 # )
 # async def send_chosen_child_job(message: Message):
